1c7701eb344b7f165b2daf113ff8eaae1819b9ed
hamzakhan947075 <hamzakhan947075@gmail.com>
2022-03-28 13:47:35 +0500

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     def create
         # debugger
    @article = Article.new(article_params)
-   @article.user= User.first
+   @article.user= current_user
    # @article.save
 # redirect_to article_path(@article)
         if @article.save

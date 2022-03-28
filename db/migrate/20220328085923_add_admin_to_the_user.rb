7a1f1b6962a78c654ae2559900c95ae5716945a1
hamzakhan947075 <hamzakhan947075@gmail.com>
2022-03-28 15:17:59 +0500

    add admin functionality

---
+++ db/migrate/20220328085923_add_admin_to_the_user.rb
@@ -0,0 +1,5 @@
+class AddAdminToTheUser < ActiveRecord::Migration
+  def change
+    add_column :users, :admin ,:boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220318103453) do
+ActiveRecord::Schema.define(version: 20220328085923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20220318103453) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
